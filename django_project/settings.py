0b340bd7e3d677ab533883f96ba20ffecf4adc52
Valentin Kunz <kunzvalentin4@gmail.com>
2025-04-21 18:17:37 +0200

    Added ALLOWED_HOSTS

--- django_project/settings.py
+++ django_project/settings.py
@@ -29,7 +29,9 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "Lieblingsmensch.pythonanywhere.com",
+]
 
 # Application definition
 
