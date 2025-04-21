c8daf358cf25784f4905e446407b94bb97572bd2
Valentin Kunz <kunzvalentin4@gmail.com>
2025-04-21 18:37:22 +0200

    Bug fixed, create new messages, a absolute path was missing, after successfully creating a message.

--- django_project/settings.py
+++ django_project/settings.py
@@ -30,6 +30,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 DEBUG = env.bool("DEBUG", default=False)
 
 ALLOWED_HOSTS = [
+    "127.0.0.1",
     "Lieblingsmensch.pythonanywhere.com",
 ]
 
--- pages/models.py
+++ pages/models.py
@@ -1,6 +1,14 @@
 from django.db import models
+from django.urls import reverse
+
 
 class Message(models.Model):
     receiver = models.CharField(max_length=20, verbose_name ="Empfänger", help_text="z.B. Max Mustermann")
     content = models.TextField(max_length=100, verbose_name ="Nachricht", help_text="z.B. Ich mag an dir...")
     created_at = models.DateTimeField(auto_now_add=True)
+    
+    def get_absolute_url(self):
+        return reverse("home")
+
+    def __str__(self):
+        return f"{self.receiver} - {self.content}"
